b628b3250a011223c8118c5f471c4356f3fcdb45
dblumenau <dblumenau@gmail.com>
2018-10-11 17:39:40 +0200

    quick update of one file on forge

--- app/Http/Controllers/RandomImageController.php
+++ app/Http/Controllers/RandomImageController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class RandomImageController extends Controller
 {
-    //
+    public function show()
+    {
+        $number = rand(1, 3);
+        $img = Image::make(public_path("$number.jpg"));
+
+        return $img->response();
+
+
+    }
 }
